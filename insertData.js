ac952fb5977890f66c41db6934ed0303f5d48cb8
Ali <alykhan027@gmail.com>
2025-09-01 22:13:50 -0400

    data collected and inserted in db

---
+++ insertData.js
@@ -0,0 +1,25 @@
+const data = require('./business-rules.json')
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb+srv://Ali:123@cluster0.iwu9tc2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dbName = "us-compliance-app";
+
+async function run() {
+    const client = new MongoClient(url);
+    try {
+        await client.connect();
+        console.log("connected");
+        const db = client.db(dbName);
+        const collection = db.collection("rules");
+
+        const result = await collection.insertMany(data);
+        console.log("Inserted documents:", result.insertedIds);
+    } catch (err) {
+        console.error("Error inserting document:", err);
+    } finally {
+        await client.close();
+    }
+}
+
+run();
+
